648ff9501bba9be76fb6733d8ae03d7f31de7703
tahir_anjum <imtahiranjum@gmail.com>
2023-04-04 22:04:56 +0500

    Initial Commit

---
+++ CalculatorApp/Form1.Designer.cs
@@ -0,0 +1,315 @@
+﻿namespace CalculatorApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.key7 = new System.Windows.Forms.Button();
+            this.key8 = new System.Windows.Forms.Button();
+            this.key9 = new System.Windows.Forms.Button();
+            this.keySlash = new System.Windows.Forms.Button();
+            this.keyCe = new System.Windows.Forms.Button();
+            this.key4 = new System.Windows.Forms.Button();
+            this.key1 = new System.Windows.Forms.Button();
+            this.key0 = new System.Windows.Forms.Button();
+            this.key6 = new System.Windows.Forms.Button();
+            this.key5 = new System.Windows.Forms.Button();
+            this.keyMultiply = new System.Windows.Forms.Button();
+            this.key2 = new System.Windows.Forms.Button();
+            this.key3 = new System.Windows.Forms.Button();
+            this.keyMinus = new System.Windows.Forms.Button();
+            this.keyDot = new System.Windows.Forms.Button();
+            this.keyPlus = new System.Windows.Forms.Button();
+            this.keyC = new System.Windows.Forms.Button();
+            this.keyEquals = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // key7
+            // 
+            this.key7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.key7.Location = new System.Drawing.Point(12, 98);
+            this.key7.Name = "key7";
+            this.key7.Size = new System.Drawing.Size(54, 48);
+            this.key7.TabIndex = 5;
+            this.key7.Text = "7";
+            this.key7.UseVisualStyleBackColor = true;
+            this.key7.Click += new System.EventHandler(this.key7_Click_1);
+            // 
+            // key8
+            // 
+            this.key8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.key8.Location = new System.Drawing.Point(72, 98);
+            this.key8.Name = "key8";
+            this.key8.Size = new System.Drawing.Size(54, 48);
+            this.key8.TabIndex = 7;
+            this.key8.Text = "8";
+            this.key8.UseVisualStyleBackColor = true;
+            this.key8.Click += new System.EventHandler(this.key8_Click);
+            // 
+            // key9
+            // 
+            this.key9.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.key9.Location = new System.Drawing.Point(132, 98);
+            this.key9.Name = "key9";
+            this.key9.Size = new System.Drawing.Size(54, 48);
+            this.key9.TabIndex = 8;
+            this.key9.Text = "9";
+            this.key9.UseVisualStyleBackColor = true;
+            this.key9.Click += new System.EventHandler(this.key9_Click);
+            // 
+            // keySlash
+            // 
+            this.keySlash.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.keySlash.Location = new System.Drawing.Point(192, 98);
+            this.keySlash.Name = "keySlash";
+            this.keySlash.Size = new System.Drawing.Size(54, 48);
+            this.keySlash.TabIndex = 9;
+            this.keySlash.Text = "/";
+            this.keySlash.UseVisualStyleBackColor = true;
+            this.keySlash.Click += new System.EventHandler(this.keySlash_Click);
+            // 
+            // keyCe
+            // 
+            this.keyCe.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.keyCe.Location = new System.Drawing.Point(252, 98);
+            this.keyCe.Name = "keyCe";
+            this.keyCe.Size = new System.Drawing.Size(57, 48);
+            this.keyCe.TabIndex = 10;
+            this.keyCe.Text = "CE";
+            this.keyCe.UseVisualStyleBackColor = true;
+            // 
+            // key4
+            // 
+            this.key4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.key4.Location = new System.Drawing.Point(12, 152);
+            this.key4.Name = "key4";
+            this.key4.Size = new System.Drawing.Size(54, 48);
+            this.key4.TabIndex = 11;
+            this.key4.Text = "4";
+            this.key4.UseVisualStyleBackColor = true;
+            this.key4.Click += new System.EventHandler(this.key4_Click);
+            // 
+            // key1
+            // 
+            this.key1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.key1.Location = new System.Drawing.Point(12, 206);
+            this.key1.Name = "key1";
+            this.key1.Size = new System.Drawing.Size(54, 48);
+            this.key1.TabIndex = 12;
+            this.key1.Text = "1";
+            this.key1.UseVisualStyleBackColor = true;
+            this.key1.Click += new System.EventHandler(this.key1_Click);
+            // 
+            // key0
+            // 
+            this.key0.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.key0.Location = new System.Drawing.Point(12, 260);
+            this.key0.Name = "key0";
+            this.key0.Size = new System.Drawing.Size(114, 48);
+            this.key0.TabIndex = 13;
+            this.key0.Text = "0";
+            this.key0.UseVisualStyleBackColor = true;
+            this.key0.Click += new System.EventHandler(this.key0_Click);
+            // 
+            // key6
+            // 
+            this.key6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.key6.Location = new System.Drawing.Point(133, 152);
+            this.key6.Name = "key6";
+            this.key6.Size = new System.Drawing.Size(54, 48);
+            this.key6.TabIndex = 14;
+            this.key6.Text = "6";
+            this.key6.UseVisualStyleBackColor = true;
+            this.key6.Click += new System.EventHandler(this.key6_Click);
+            // 
+            // key5
+            // 
+            this.key5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.key5.Location = new System.Drawing.Point(72, 152);
+            this.key5.Name = "key5";
+            this.key5.Size = new System.Drawing.Size(54, 48);
+            this.key5.TabIndex = 15;
+            this.key5.Text = "5";
+            this.key5.UseVisualStyleBackColor = true;
+            this.key5.Click += new System.EventHandler(this.key5_Click);
+            // 
+            // keyMultiply
+            // 
+            this.keyMultiply.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.keyMultiply.Location = new System.Drawing.Point(192, 152);
+            this.keyMultiply.Name = "keyMultiply";
+            this.keyMultiply.Size = new System.Drawing.Size(54, 48);
+            this.keyMultiply.TabIndex = 16;
+            this.keyMultiply.Text = "*";
+            this.keyMultiply.UseVisualStyleBackColor = true;
+            this.keyMultiply.Click += new System.EventHandler(this.keyMultiply_Click);
+            // 
+            // key2
+            // 
+            this.key2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.key2.Location = new System.Drawing.Point(73, 206);
+            this.key2.Name = "key2";
+            this.key2.Size = new System.Drawing.Size(54, 48);
+            this.key2.TabIndex = 17;
+            this.key2.Text = "2";
+            this.key2.UseVisualStyleBackColor = true;
+            this.key2.Click += new System.EventHandler(this.key2_Click);
+            // 
+            // key3
+            // 
+            this.key3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.key3.Location = new System.Drawing.Point(133, 206);
+            this.key3.Name = "key3";
+            this.key3.Size = new System.Drawing.Size(54, 48);
+            this.key3.TabIndex = 18;
+            this.key3.Text = "3";
+            this.key3.UseVisualStyleBackColor = true;
+            this.key3.Click += new System.EventHandler(this.key3_Click);
+            // 
+            // keyMinus
+            // 
+            this.keyMinus.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.keyMinus.Location = new System.Drawing.Point(192, 206);
+            this.keyMinus.Name = "keyMinus";
+            this.keyMinus.Size = new System.Drawing.Size(54, 48);
+            this.keyMinus.TabIndex = 19;
+            this.keyMinus.Text = "-";
+            this.keyMinus.UseVisualStyleBackColor = true;
+            this.keyMinus.Click += new System.EventHandler(this.keyMinus_Click);
+            // 
+            // keyDot
+            // 
+            this.keyDot.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.keyDot.Location = new System.Drawing.Point(132, 260);
+            this.keyDot.Name = "keyDot";
+            this.keyDot.Size = new System.Drawing.Size(54, 48);
+            this.keyDot.TabIndex = 20;
+            this.keyDot.Text = ".";
+            this.keyDot.UseVisualStyleBackColor = true;
+            this.keyDot.Click += new System.EventHandler(this.keyDot_Click);
+            // 
+            // keyPlus
+            // 
+            this.keyPlus.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.keyPlus.Location = new System.Drawing.Point(192, 260);
+            this.keyPlus.Name = "keyPlus";
+            this.keyPlus.Size = new System.Drawing.Size(54, 48);
+            this.keyPlus.TabIndex = 21;
+            this.keyPlus.Text = "+";
+            this.keyPlus.UseVisualStyleBackColor = true;
+            this.keyPlus.Click += new System.EventHandler(this.keyPlus_Click);
+            // 
+            // keyC
+            // 
+            this.keyC.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.keyC.Location = new System.Drawing.Point(252, 152);
+            this.keyC.Name = "keyC";
+            this.keyC.Size = new System.Drawing.Size(54, 48);
+            this.keyC.TabIndex = 22;
+            this.keyC.Text = "C";
+            this.keyC.UseVisualStyleBackColor = true;
+            this.keyC.Click += new System.EventHandler(this.keyC_Click);
+            // 
+            // keyEquals
+            // 
+            this.keyEquals.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.keyEquals.Location = new System.Drawing.Point(252, 206);
+            this.keyEquals.Name = "keyEquals";
+            this.keyEquals.Size = new System.Drawing.Size(54, 102);
+            this.keyEquals.TabIndex = 23;
+            this.keyEquals.Text = "=";
+            this.keyEquals.UseVisualStyleBackColor = true;
+            this.keyEquals.Click += new System.EventHandler(this.keyEquals_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 31);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(297, 46);
+            this.textBox1.TabIndex = 24;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(321, 317);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.keyEquals);
+            this.Controls.Add(this.keyC);
+            this.Controls.Add(this.keyPlus);
+            this.Controls.Add(this.keyDot);
+            this.Controls.Add(this.keyMinus);
+            this.Controls.Add(this.key3);
+            this.Controls.Add(this.key2);
+            this.Controls.Add(this.keyMultiply);
+            this.Controls.Add(this.key5);
+            this.Controls.Add(this.key6);
+            this.Controls.Add(this.key0);
+            this.Controls.Add(this.key1);
+            this.Controls.Add(this.key4);
+            this.Controls.Add(this.keyCe);
+            this.Controls.Add(this.keySlash);
+            this.Controls.Add(this.key9);
+            this.Controls.Add(this.key8);
+            this.Controls.Add(this.key7);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.TextBox textBox1;
+
+        private System.Windows.Forms.Button keyMinus;
+        private System.Windows.Forms.Button keyDot;
+        private System.Windows.Forms.Button keyPlus;
+        private System.Windows.Forms.Button keyC;
+        private System.Windows.Forms.Button keyEquals;
+
+        private System.Windows.Forms.Button key5;
+        private System.Windows.Forms.Button keyMultiply;
+        private System.Windows.Forms.Button key2;
+        private System.Windows.Forms.Button key3;
+
+        private System.Windows.Forms.Button key6;
+
+        private System.Windows.Forms.Button key4;
+        private System.Windows.Forms.Button key1;
+        private System.Windows.Forms.Button key0;
+
+        private System.Windows.Forms.Button key8;
+        private System.Windows.Forms.Button key9;
+        private System.Windows.Forms.Button keySlash;
+        private System.Windows.Forms.Button keyCe;
+
+        private System.Windows.Forms.Button key7;
+    }
+}
---
+++ CalculatorApp/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+
+using System.Windows.Forms;
+
+namespace CalculatorApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        float _num1, _ans;
+        int _count;
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+        }
+
+        private void key1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "1";
+        }
+
+        private void key8_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "8";
+        }
+
+        private void key9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "9";
+        }
+
+        private void key4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "4";
+        }
+
+        private void key5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "5";
+        }
+
+        private void key6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "6";
+        }
+
+        private void key2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "2";
+        }
+
+        private void key3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "3";
+        }
+
+        private void key0_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "0";
+        }
+        
+
+        private void keyDot_Click(object sender, EventArgs e)
+        {
+            int c = textBox1.TextLength;
+            int flag = 0;
+            string text = textBox1.Text;
+            for (int i = 0; i < c; i++)
+            {
+                if (text[i].ToString() == ".")
+                {
+                    flag = 1; break;
+                }
+                else
+                {
+                    flag = 0;
+                }
+            }
+            if (flag == 0)
+            {
+                textBox1.Text += ".";
+            }
+        }
+
+        private void keyPlus_Click(object sender, EventArgs e)
+        {
+            _num1 = float.Parse(textBox1.Text);
+            textBox1.Clear();
+            textBox1.Focus();
+            _count = 2;
+        }
+
+        private void keyEquals_Click(object sender, EventArgs e)
+        {
+            Compute(_count);
+        }
+
+        private void keyC_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            _count = 0;
+        }
+        
+        private void Compute(int count)
+        {
+            switch (count)
+            {
+                case 1:
+                    _ans = _num1 - float.Parse(textBox1.Text);
+                    textBox1.Text = _ans.ToString();
+                    break;
+                case 2:
+                    _ans = _num1 + float.Parse(textBox1.Text);
+                    textBox1.Text = _ans.ToString();
+                    break;
+                case 3:
+                    _ans = _num1 * float.Parse(textBox1.Text);
+                    textBox1.Text = _ans.ToString();
+                    break;
+                case 4:
+                    _ans = _num1 / float.Parse(textBox1.Text);
+                    textBox1.Text = _ans.ToString();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void keySlash_Click(object sender, EventArgs e)
+        {
+            _num1 = float.Parse(textBox1.Text);
+            textBox1.Clear();
+            textBox1.Focus();
+            _count = 4;
+        }
+
+        private void keyMultiply_Click(object sender, EventArgs e)
+        {
+            _num1 = float.Parse(textBox1.Text);
+            textBox1.Clear();
+            textBox1.Focus();
+            _count = 3;
+        }
+
+        private void keyMinus_Click(object sender, EventArgs e)
+        {
+            _num1 = float.Parse(textBox1.Text);
+            textBox1.Clear();
+            textBox1.Focus();
+            _count = 1;
+        }
+
+        private void key7_Click_1(object sender, EventArgs e)
+        {
+            textBox1.Text += "7";
+        }
+    }
+}
